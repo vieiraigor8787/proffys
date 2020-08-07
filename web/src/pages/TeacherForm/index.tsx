7979653fa65f2dcf0890b65ff3db43c7898e38d0
Igor Vieira de Cerqueira <igor_vc@hotmail.com>
2020-08-07 20:28:52 -0300

    component input,update form/list

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import PageHeader from '../../components/PageHeader';
 
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 import './styles.css';
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-           <PageHeader title="Que incrível que você quer dar aulas!"/>
+           <PageHeader 
+           title="Que incrível que você quer dar aulas!"
+           description="o primeiro passo para fazer a diferença"
+           />
+
+           <main>
+               <fieldset>
+                   <legend>Seus dados</legend>
+                   <Input name="name" label="name"/>
+                   <Input name="avatar" label="avatar"/>
+                   <Input name="whatsapp" label="whatsapp"/>
+               </fieldset>
+
+               <fieldset>
+                   <legend>Sobre a aula</legend>
+                   <Input name="subject" label="subject"/>
+                   <Input name="cost" label="avatar"/>
+               </fieldset>
+
+               <footer>
+                   <p>
+                       <img src={warningIcon} alt="aviso" />
+                       Importante! <br/>
+                       Preencha todos os dados.
+                   </p>
+                   <button type="button">Salvar cadastro</button>
+               </footer>
+           </main>
         </div>
     )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -10,18 +11,9 @@ const TeacherList: React.FC = () => {
 		<div id='page-teacher-list' className='container'>
 			<PageHeader title='Estes são os proffys disponíveis'>
 				<form id='search-teachers'>
-					<div className='input-block'>
-						<label htmlFor='subject'>Matéria</label>
-						<input type='text' id='subject' />
-					</div>
-					<div className='input-block'>
-						<label htmlFor='week_day'>Dia da semana</label>
-						<input type='text' id='week_day' />
-					</div>
-					<div className='input-block'>
-						<label htmlFor='time'>Matéria</label>
-						<input type='text' id='time' />
-					</div>
+					<Input name="subject" label="subject" />
+					<Input name="week_day" label="week_day" />
+					<Input name="time" label="time" type="time"/>
 				</form>
 			</PageHeader>
 
