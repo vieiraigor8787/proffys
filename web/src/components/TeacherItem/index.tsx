7fa7075d0260a67d5d560ffd1d3f099f42b1b399
Igor Vieira de Cerqueira <igor_vc@hotmail.com>
2020-08-09 19:35:22 -0300

    .

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,35 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+	id: string;
+	avatar: string;
+	bio: string;
+	cost: number;
+	name: string;
+	subject: string;
+	whatsapp: string;
+}
+
+interface TeacherItemProps {
+	teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id
+		})
+	}
+
 	return (
 		<article className='teacher-item'>
 			<header>
 				<img
-					src='https://api.adorable.io/avatars/285/abott@adorable.png'
+					src={teacher.avatar}
 					alt='avatar'
 				/>
 				<div>
-					<strong>Mario Silva</strong>
-					<span>Química</span>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
 				</div>
 			</header>
-			<p>
-				Quis officia voluptate proident occaecat Lorem id enim nulla aliquip
-				veniam nostrud elit eiusmod. Nostrud anim cupidatat nulla eiusmod magna
-				consectetur officia cupidatat et laboris sint velit. Irure qui culpa
-				dolore nostrud mollit deserunt Lorem sunt irure est elit.
-			</p>
+			<p>{teacher.bio}</p>
 			<footer>
 				<p>
 					Preço/hora:
-					<strong>R$ 80,00</strong>
+					<strong>R$ {teacher.cost}</strong>
 				</p>
-				<button type='button'>
+				<a target='_blank' onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
 					<img src={whatsappIcon} alt='whatsapp' />
 					Entrar em contato
-				</button>
+				</a>
 			</footer>
 		</article>
 	);
